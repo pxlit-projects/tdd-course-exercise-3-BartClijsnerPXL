060f6fada3833a66c6920528a8a38fb32fbfbaa2
Tim Schraepen <sch3lp@gmail.com>
2016-12-06 14:27:18 +0100

    initial classes

---
+++ src/main/java/be/pxl/tdd/SpreadSheet.java
@@ -0,0 +1,4 @@
+package be.pxl.tdd;
+
+public class SpreadSheet {
+}
---
+++ src/test/java/be/pxl/tdd/SpreadSheetTest.java
@@ -0,0 +1,13 @@
+package be.pxl.tdd;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SpreadSheetTest {
+
+    @Test
+    public void name() throws Exception {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
